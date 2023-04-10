fe30ddc17323e90a2158b8f109573c65e9469c9f
papc92 <118466089+papc92@users.noreply.github.com>
2023-04-10 11:50:02 -0500

    R

---
+++ doc_DATASET.R
@@ -0,0 +1,13 @@
+#' DATASET
+#'
+#' A small example dataset.
+#'
+#' @format A data frame with 30 rows and 4 variables:
+#' \describe{
+#'   \item{ age }{  character }
+#'   \item{ primary_fur_color }{  character }
+#'   \item{ activity }{  character }
+#'   \item{ counts }{  numeric }
+#' }
+#' @source Squirrels data
+"DATASET"
